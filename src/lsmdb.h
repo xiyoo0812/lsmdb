094d23bc3aba9f2e08ca9046479f9092d0a5b970
xiyoo0812 <xiyoo0812@163.com>
2024-12-24 16:54:25 +0800

    优化文本输出

--- src/lsmdb.h
+++ src/lsmdb.h
@@ -105,7 +105,7 @@ namespace lsmdb {
             size_t len;
             int type = lua_type(L, idx);
             if (type != LUA_TSTRING && type != LUA_TNUMBER) {
-                luaL_error(L, "lsmdb read %d type %s not suppert!", idx, lua_typename(L, idx));
+                luaL_error(L, "lsmdb read key %d type %s not suppert!", idx, lua_typename(L, idx));
             }
             if (m_codec) {
                 const char* buf = (const char*)m_codec->encode(L, idx, &len);
@@ -129,12 +129,12 @@ namespace lsmdb {
                     return string_view(buf, len);
                 }
                 default:
-                    luaL_error(L, "lsmdb read %d type %s not suppert!", idx, lua_typename(L, idx));
+                    luaL_error(L, "lsmdb read value %d type %s not suppert!", idx, lua_typename(L, idx));
                     break;
                 }
             }
             if (type != LUA_TSTRING && type != LUA_TNUMBER) {
-                luaL_error(L, "lsmdb read %d type %s not suppert!", idx, lua_typename(L, idx));
+                luaL_error(L, "lsmdb read value %d type %s not suppert!", idx, lua_typename(L, idx));
             }
             const char* buf = lua_tolstring(L, idx, &len);
             return string_view(buf, len);
